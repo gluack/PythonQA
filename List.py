37cc37860d736ba2174c3eed39fd7b6cf466001c
gal.sem_W31 <gluack@gmail.com>
2020-01-27 19:24:05 +0200

    Initial commit

---
+++ List.py
@@ -0,0 +1,5 @@
+import random
+y= (random.sample(range(0, 10), 10))
+print (y)
+x=y[:-1]
+print(x)
---
+++ List2.py
@@ -0,0 +1,18 @@
+import random
+a=(random.randint(0,10))
+print ("a=",a)
+b=(random.randint(0,10))
+print ("b=",b)
+c=(random.randint(0,10))
+if a>b:
+  print("Good boy")
+elif a<b:
+  print("Sorrow")
+elif a==b:
+  print("Try this one: c=",c)
+  if a+b <c:
+       print("Super")
+  elif a+b >c:
+       print("Badly")
+  elif a+b==c:
+       print("Страдания")
