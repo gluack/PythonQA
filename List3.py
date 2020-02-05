c6ff1057ba58cf8ecf75a41d007bd80ced9dfa60
Halyna <gluack@gmail.com>
2020-02-05 21:19:06 +0200

    rearange

---
+++ List3.py
@@ -0,0 +1,18 @@
+from random import randint
+
+x = (int(input('Довжина списку = ')))
+y = (int(input('Найбільше значення списку = ')))
+
+if x == 0:
+  print('Ах ти бешкетник довжина має бути більше 0, запускай ще раз!')
+
+def Bill(a,b):)
+  ab = []
+  for i in range(a):
+	  ab.append(randint(0,b))
+  return ab
+
+list_A=Bill(x,y)
+
+
+print(list_A
